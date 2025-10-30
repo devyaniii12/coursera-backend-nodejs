11fc38f332eb099d21a04624eac4882f4d1f82f1
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-10-31 02:15:07 +0530

    Update public_users.js

--- backend-apps/public_users.js
+++ backend-apps/public_users.js
@@ -1,1 +1,9 @@
-public_users.get('/async', async function (req, res) {  try {    const bookList = await getBookListAsync('http://localhost:5000/'); //    res.json(bookList);  } catch (error) {    console.error(error);    res.status(500).json({ message: "Error retrieving book list" });  }}); 
+public_users.get('/async', async function (req, res) {
+  try {
+    const bookList = await getBookListAsync('http://localhost:5000/');
+    res.json(bookList);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving book list" });
+  }
+});
